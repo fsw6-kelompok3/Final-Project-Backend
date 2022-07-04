abacfda154ee02483edfebbc2286dd5b95036da0
aurellieandra <diandra.azra@gmail.com>
2022-07-04 18:57:15 +0700

    trial heroku attempt

---
+++ config/database.js
@@ -0,0 +1,40 @@
+const {
+  DB_USER = "aurellieandra",
+  DB_PASSWORD = "123456",
+  DB_NAME = "sewa_buku",
+  DB_HOST = "127.0.0.1",
+  DB_PORT = "5432",
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_development`,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres"
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_test`,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres"
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}`,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }
+}
